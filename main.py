9e700088380a11c7f409ae6a46855bbdef68bf69
Yukamikey <38221217+Yukamikey@users.noreply.github.com>
2024-03-10 12:03:13 +0000

    implementing ui

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from coc_modules import  *
+from ui import *
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+
+class MyGui(QMainWindow):
+
+    def __int__(self):
+        super(MyGui, self).__init__()
+        uic.loadUi("econ_planner_ui.ui", self)
+        self.show()
+def main():
+    app = QApplication([])
+    window = MyGui()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
